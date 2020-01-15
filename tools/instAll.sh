ab7b5bbaf7510eed3e438c0f186335928c244720
Rodrigo Estrella <rodrigoe@xioio.com>
2020-01-15 14:23:55 -0800

    replace breaking chars from profile name

--- tools/instAll.sh
+++ tools/instAll.sh
@@ -31,7 +31,7 @@ install_scheme () {
     # Add the profile to the Terminal plist.
     # Window Settings is a dictionary of installed terminal profiles. 
     XML=$(xmllint --xpath '/plist/dict' "$1")
-    NAME=$(echo $1 | awk '{split($0,a,"../schemes/"); print a[2]}' | cut -d'.' -f1)
+    NAME=$(echo $1 | awk '{split($0,a,"../schemes/"); print a[2]}'| sed -e 's/(//g' | sed -e 's/)//g' | cut -d'.' -f1)
     defaults write com.apple.Terminal "Window Settings" -dict-add "$NAME" "$XML"
 }
 
